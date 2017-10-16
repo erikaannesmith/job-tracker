140d4a8fda0a5b916d089874c6a5895d74ed23c5
Erika Smith <erikaannesmith@gmail.com>
2017-10-15 18:50:42 -0600

    Create new and form pages, new method is working but create is not

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,42 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.name} added!"
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
+end
